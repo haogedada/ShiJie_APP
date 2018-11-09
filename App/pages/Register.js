36d841a181e6f01c63ae9dc609da11140538af08
haogedada <2205584662@qq.com>
2018-11-09 15:34:24 +0800

    同步优化注册功能

--- App/pages/Register.js
+++ App/pages/Register.js
@@ -14,12 +14,12 @@ class Register extends Component {
             isRegister: false,
             prompt: ' ',
             random: randomNumber(),
-            btn_disabled: true,
+            promptCount: 0,
         }
     }
     render() {
         let prompt_str = [
-            '用户名为长度3至20的英文字母',
+            '用户名为长度3至20的英文字母数字组合',
             '密码为长度8至16的英文字符组合',
             '两次密码不匹配',
             '邮箱不合法',
@@ -32,39 +32,34 @@ class Register extends Component {
          * @param {*} e 
          */
         function btn_register(e) {
-            if (!btn_disabled) {
-                let form = {
-                    username: this.state.username,
-                    password: this.state.password,
-                    email: this.state.email
-                }
-                register(form).then(response => {
-                    if (response.code === 200) {
-                        Alert.alert('注册成功')
-                    } else if (response.code === 500) {
-                        Alert.alert(response.msg)
-                    }
-                });
-            } else {
-                Alert.alert('请正确输入信息')
+            let form = {
+                username: this.state.username,
+                password: this.state.password,
+                email: this.state.email
             }
-
+            register(form).then(response => {
+                if (response.code === 200) {
+                    Alert.alert('注册成功')
+                } else if (response.code === 500) {
+                    Alert.alert(response.msg)
+                }
+            });
         }
         /**
          * 
          * @param {验证邮箱是否存在} e 
          */
-        function handleEmail(e) {
+        function handlePromptEmail(e) {
             const re = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
             if (!re.test(this.state.email)) {
-                this.setState({ btn_disabled: true, prompt: prompt_str[3] });
+                this.setState({ prompt: prompt_str[3] });
             } else {
                 let _email = { email: this.state.email }
                 promptEmail(_email).then(response => {
                     if (response.code === 200) {
-                        this.setState({ btn_disabled: true, prompt: prompt_str[4] });
+                        this.setState({ prompt: prompt_str[4] });
                     } else {
-                        this.setState({ prompt: ' ' });
+                        this.setState({ prompt: ' ', promptCount: this.state.promptCount + 1 });
                     }
                 })
             }
@@ -72,48 +67,61 @@ class Register extends Component {
         /**
          * 用户名验证
          */
-        function handleUserName(e) {
-            const re = /[^A-Za-z]/;
-            if (((3 > this.state.username.length) ||
-                (this.state.username.length > 20))) {
-                this.setState({ btn_disabled: true, prompt: prompt_str[0] });
-            } else if (re.test(this.state.username)) {
-                this.setState({ btn_disabled: true, prompt: prompt_str[0] });
+        function handlePromptUserName(e) {
+            const re = /^[0-9a-zA-z]/;
+            if ((3 > this.state.username.length) ||
+                (this.state.username.length > 20) ||
+                !re.test(this.state.username)) {
+                this.setState({ prompt: prompt_str[0] });
             } else {
                 if (this.state.username !== '  ') {
                     let _username = { username: this.state.username }
                     promptUserName(_username).then(response => {
-                        console.log(response);
-
                         if (response.code === 200) {
-                            this.setState({ btn_disabled: true, prompt: prompt_str[6] });
+                            this.setState({ prompt: prompt_str[6] });
                         } else {
-                            this.setState({ prompt: ' ' });
+                            this.setState({ prompt: ' ', promptCount: this.state.promptCount + 1 });
                         }
                     })
                 } else {
-                    this.setState({ btn_disabled: true, prompt: '用户名不能为空' });
+                    this.setState({ prompt: '用户名不能为空' });
                 }
             }
         }
         /**
-         * 
-         * @param {表单参数验证} e 
+         * 密码验证
          */
-
-        function handlePrompt(e) {
-            if ((8 > this.state.password.length) || (this.state.password.length > 16)) {
-                this.setState({ btn_disabled: true, prompt: prompt_str[1] });
-            } else if (this.state.password !== (this.state.okpassword)) {
-                this.setState({ btn_disabled: true, prompt: prompt_str[2] });
-            } else if ((this.state.yzm !== this.state.random.toLowerCase()) &&
-                (this.state.yzm !== this.state.random.toUpperCase())) {
-                this.setState({ btn_disabled: true, prompt: prompt_str[5] });
+        function handlePromptPassword(e) {
+            const re = /^[\u3220-\uFA29]+$/;
+            if ((re.test(this.state.password)) ||
+                (8 > this.state.password.length) ||
+                (this.state.password.length > 16)) {
+                this.setState({ prompt: prompt_str[1] });
             } else {
-                this.setState({
-                    btn_disabled: false,
-                    prompt: ' '
-                });
+                this.setState({ prompt: ' ', promptCount: this.state.promptCount + 1 });
+            }
+
+        }/**
+         * 确认密码验证
+         * @param {*} e 
+         */
+        function handlePromptOkPassword(e) {
+            if (this.state.password !== (this.state.okpassword)) {
+                this.setState({ prompt: prompt_str[2] });
+            } else {
+                this.setState({ prompt: ' ', promptCount: this.state.promptCount + 1 });
+            }
+        }
+        /**
+         * 验证码验证
+         * @param {*} e 
+         */
+        function handlePromptYzm(e) {
+            if ((this.state.yzm !== this.state.random.toLowerCase()) &&
+                (this.state.yzm !== this.state.random.toUpperCase())) {
+                this.setState({ prompt: prompt_str[5] });
+            } else {
+                this.setState({ prompt: ' ', promptCount: this.state.promptCount + 1 });
             }
         }
         return (
@@ -123,14 +131,14 @@ class Register extends Component {
                     <TextInput style={styles.input}
                         onChangeText={(value) => this.setState({ username: value })}
                         placeholder="请输入用户名"
-                        onBlur={handleUserName.bind(this)} />
+                        onBlur={handlePromptUserName.bind(this)} />
                 </View>
                 <View style={styles.row}>
                     <Text>密码</Text>
                     <TextInput style={styles.input}
                         onChangeText={(value) => this.setState({ password: value })}
                         placeholder="请输入密码"
-                        onBlur={handlePrompt.bind(this)}
+                        onBlur={handlePromptPassword.bind(this)}
                     />
                 </View>
                 <View style={styles.row}>
@@ -138,20 +146,20 @@ class Register extends Component {
                     <TextInput style={styles.input}
                         onChangeText={(value) => this.setState({ okpassword: value })}
                         placeholder="请输入密码"
-                        onBlur={handlePrompt.bind(this)} />
+                        onBlur={handlePromptOkPassword.bind(this)} />
                 </View>
                 <View style={styles.row}>
                     <Text>邮箱</Text>
                     <TextInput style={styles.input}
                         onChangeText={(value) => this.setState({ email: value })}
-                        onBlur={handleEmail.bind(this)}
+                        onBlur={handlePromptEmail.bind(this)}
                         placeholder="请输入邮箱" />
                 </View>
                 <View style={styles.row}>
                     <Text>验证码</Text>
                     <TextInput style={styles.input}
                         onChangeText={(value) => this.setState({ yzm: value })}
-                        onBlur={handlePrompt.bind(this)}
+                        onBlur={handlePromptYzm.bind(this)}
                         placeholder="请输入验证码" />
                     <Text onPress={() => { this.setState({ random: randomNumber() }) }}>{this.state.random}</Text>
                 </View>
@@ -160,7 +168,9 @@ class Register extends Component {
                 </View>
                 <View style={styles.row}>
                     <View style={styles.btn} >
-                        <Button title="注册" disabled={this.state.btn_disabled} style={styles.btn} onPress={btn_register.bind(this)} />
+                        <Button title="注册"
+                            disabled={this.state.promptCount === 5 ? false : true}
+                            style={styles.btn} onPress={btn_register.bind(this)} />
                     </View>
                 </View>
             </View>
--- App/util/random.js
+++ App/util/random.js
@@ -2,7 +2,7 @@ export const randomNumber = () => {
     let str = 'abcdefghjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
     let str_='';
     for (let i = 0; i < 6; i++) {
-      let num = parseInt(Math.random()*(62+1),10);
+      let num = parseInt(Math.random()*(61),10);
       str_ += str.substring(num,num+1)
     }
    return str_;
