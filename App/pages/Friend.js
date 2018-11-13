1581a4b020c5c8450c4ba066c6f3e8981bc265bf
Net-Home <liuwu4@yeah.net>
2018-11-13 12:52:46 +0800

    完善朋友页面

--- App/pages/Friend.js
+++ App/pages/Friend.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react'
+import React, {
+  Component
+} from 'react'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
 import FansAndConcernContain from '../components/friend/FansAndConcernContain';
-import { getUserFansList, getUserFollowList } from '../netWork/api'
-import { Alert } from 'react-native'
+import {
+  getUserFansList,
+  getUserFollowList
+} from '../netWork/api'
+import {
+  Alert
+} from 'react-native'
 export default class Friend extends Component {
   constructor(props) {
     super(props)
@@ -18,47 +25,64 @@ export default class Friend extends Component {
   componentDidMount() {
 
   }
-    loadUserFriend() {
-      getUserFollowList().then(_followList=>{
-        let newFollowList = [];
-        _followList.data.forEach(item => {
-          newFollowList.push({
-            id: item.userId,
-            name: item.userNickname,
-            signature: item.bardianSign,
-            avatar: item.headimgUrl
-          })
-        });
-        console.log(_followList);
-        this.setState({ followList: newFollowList })
-        
-      }).catch(err=>{
-        Alert.alert(err)
+  loadUserFriend() {
+    getUserFollowList().then(_followList => {
+      let newFollowList = [];
+      _followList.data.forEach(item => {
+        newFollowList.push({
+          id: item.userId,
+          name: item.userNickname,
+          signature: item.bardianSign,
+          avatar: item.headimgUrl
+        })
+      });
+      console.log(_followList);
+      this.setState({
+        followList: newFollowList
       })
-      getUserFansList().then(_fansList=>{
-        let newFansList = []
-        _fansList.data.forEach(item => {
-          newFansList.push({
-            id: item.userId,
-            name: item.userNickname,
-            signature: item.bardianSign,
-            avatar: item.headimgUrl
-          })
-        });
-        this.setState({fansList:newFansList})
-      }).catch(err=>{
-        Alert.alert(err)
+
+    }).catch(err => {
+      Alert.alert(err)
+    })
+    getUserFansList().then(_fansList => {
+      let newFansList = []
+      _fansList.data.forEach(item => {
+        newFansList.push({
+          id: item.userId,
+          name: item.userNickname,
+          signature: item.bardianSign,
+          avatar: item.headimgUrl
+        })
+      });
+      this.setState({
+        fansList: newFansList
       })
-  
+    }).catch(err => {
+      Alert.alert(err)
+    })
+
   }
 
   render() {
     this.loadUserFriend
-    return (
-      <ScrollableTabView tabBarUnderlineStyle={{ backgroundColor: '#1296db' }} tabBarActiveTextColor='#1296db'>
-        <FansAndConcernContain data={this.state.fansList} tabLabel="粉丝" />
-        <FansAndConcernContain data={this.state.followList} tabLabel="关注" />
-      </ScrollableTabView>
+    return ( < ScrollableTabView tabBarUnderlineStyle = {
+        {
+          backgroundColor: '#1296db'
+        }
+      }
+      tabBarActiveTextColor = '#1296db' >
+      <
+      FansAndConcernContain data = {
+        this.state.fansList
+      }
+      tabLabel = "粉丝" / >
+      <
+      FansAndConcernContain data = {
+        this.state.followList
+      }
+      tabLabel = "关注" / >
+      <
+      /ScrollableTabView>
     )
   }
 }
