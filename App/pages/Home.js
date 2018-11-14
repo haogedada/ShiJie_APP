6be5311249adc7ffaa70e1d4b91b8ce8c26b2b7f
Net-Home <liuwu4@yeah.net>
2018-11-14 21:12:33 +0800

    修改首页

--- App/pages/Home.js
+++ App/pages/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Text, View, ScrollView, TouchableOpacity, Image, Button, FlatList, WebView} from 'react-native'
+import {Text, View, ScrollView,TouchableOpacity, Image, Button, FlatList, WebView,RefreshControl} from 'react-native'
 import Storagge from '../util/AsyncStorageUtil'
 import {getVideo} from "../netWork/api"
 
@@ -9,111 +9,45 @@ export default class Home extends Component {
         this.state = {
             videoData: "",
             videoUser: "",
+            isRef:false
         };
-        this.dataSource = [{
-            key: 1,
-            value: '数据1'
-        }
-            ,
-
-            {
-                key: 10,
-                value: '数据1'
-            }
-            ,
-
-            {
-                key: 0,
-                value: '数据1'
-            }
-            ,
-
-            {
-                key: 2,
-                value: '数据1'
-            }
-            ,
-
-            {
-                key: 3,
-                value: '数据1'
-            }
-            ,
-
-            {
-                key: 4,
-                value: '数据1'
-            }
-            ,
-
-            {
-                key: 5,
-                value: '数据1'
-            }
-            ,
-
-            {
-                key: 6,
-                value: '数据1'
-            }
-            ,
-
-            {
-                key: 7,
-                value: '数据1'
-            }
-            ,
-
-            {
-                key: 8,
-                value: '数据1'
-            }
-            ,
-
-            {
-                key: 9,
-                value: '数据1'
-            }
-        ]
     }
 
-    async componentDidMount() {
-        let saveVideo = await Storagge.get("videoInfo");
-       // getVideo(2);
-        this.setState({
-            videoData: saveVideo.data,
-            videoUser: this.state.videoData.userBean
+   componentWillMount(){
+      this.video();
+   }
+   video(){
+        getVideo().then(req=>{
+            console.log("视频:",req);
         });
-      //  console.log(saveVideo);
-    }
-
+   }
+   onRef(){
+       this.setState({
+           isRef:true
+       });
+       setTimeout(()=>{
+            this.loading();
+           this.setState({
+               isRef:false
+           });
+       },2000)
+   }
+loading(){
+    alert("刷新");
+}
     render() {
         return (
             <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <ScrollView onScroll={
-                    () => {
-                        console.log('刷新');
-                    }
-                }>
-                    <FlatList renderItem={
-                        (item) => {
-                            return (<Text
-                                style={{
-                                    height: 40,
-                                    lineHeight: 40,
-                                    marginTop: 10,
-                                    backgroundColor: "#ff5fb2",
-                                    color: "#fff"
-
-                                }}
-                            >item</Text>);
-                        }
-                    }
-                              data={this.dataSource}
-                              ItemSeparatorComponent={() => {
-                                  return (<View></View>);
-                              }}
-                    />
+                <ScrollView  refreshControl={<RefreshControl
+                refreshing={this.setState.isRef}
+                onRefresh={()=>this.onRef()}
+                tintColor='gary'
+                />}>
+                  <View>
+                      <Text>1231546131</Text>
+                      <Text>1231546131</Text>
+                      <Text>1231546131</Text>
+                  </View>
                 </ScrollView>
             </View>
         )
