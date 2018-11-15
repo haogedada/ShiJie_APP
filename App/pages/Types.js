5ea1d2dcc85614e8001551585f752b8440531c45
Net-Home <liuwu4@yeah.net>
2018-11-15 18:03:39 +0800

    更改type标题导航栏

--- App/pages/Types.js
+++ App/pages/Types.js
@@ -23,34 +23,54 @@ export default class Classification extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            typeList: [],
-            //所有视屏
-            videoTypeList: [],
-            //分类视屏
-            videoList: [],
-            isRef: false
+            typeKey: [],
+            typeValue: [],
+            isRef: false,
+            selectColor: []
+        }
+        ;
 
-        };
-
+        this.selectedIndex = 0
     }
 
     componentWillMount() {
-// 标题
+        // 标题
         getTypes().then(item => {
-            let keyValue = [];
+            //key
+            let titleKey = [];
+            //value
+            let titleValue = [];
             console.log(item.data);
             let type = item.data;
             for (let i of type) {
                 // console.log("类型:", i);
                 let spilt = i.split(":");
                 // console.log("拆分:", spilt[0], spilt[1]);
-                keyValue.push({
-                    id: spilt[0],
-                    name: spilt[1]
-                })
+                titleKey.push(spilt[0])
+                titleValue.push(spilt[1])
             }
+            let sortKey = [];
+            let sortValue = [];
+            let userSelectColor = [];
+            for (let i = 0; i < titleValue.length; i++) {
+                if (i === 0) {
+                    userSelectColor.push("#9ec6ff")
+                } else {
+                    userSelectColor.push("#ff5fb2")
+                }
+            }
+
+            //倒序
+            for (let i = titleValue.length - 1; i > 0; i--) {
+
+                sortKey.push(titleKey[i]);
+                sortValue.push(titleValue[i])
+            }
+
             this.setState({
-                typeList: keyValue
+                typeKey: sortKey,
+                typeValue: sortValue,
+                selectColor: userSelectColor
             });
         });
         //视屏
@@ -66,24 +86,48 @@ export default class Classification extends Component {
      * @returns {*}
      */
     titleData() {
-        let title = this.state.typeList;
-        return title.map(item => {
-            // console.log(item.id);ooo
-            return (<Text onPress={() => {
-                this.titleClick(item);
-            }} key={item.id} style={classStyle.classTitleText}>{item.name}</Text>);
-        });
-    }
+        let key = this.state.typeKey;
+        let value = this.state.typeValue;
+        let color = this.state.selectColor;
+        let tipsArray = new Array();
 
+        for (let i = 0; i < key.length; i++) {
+            let index = i;
+            let element = (<Text
+                onPress={() => {
+                    let selecteds = new Array();
+                    for (let i = 0; i < value.length; i++) {
+                        if (i === index) {
+                            selecteds.push("rgb(0,0,0)")
+                        } else {
+                            selecteds.push("rgb(111,111,111)")
+                        }
+                    }
+                    this.selectedIndex = index;
+                    this.setState({
+                        userSelectColor: selecteds
+                    });
+                    if (index < key - 4) {
+                        this.refs.scrollView.scrollTo({x: width / 4 * index, animated: true})
+                    } else {
+                        this.refs.scrollView.scrollToEnd({animated: true})
+                    }
+                }
+                }
+                key={index} style={[classStyle.classTitleText, {color: this.state.selectColor[index]}]}
+            >
+                {value[i]}
+            </Text>);
+            tipsArray.push(element)
+        }
+        return tipsArray
+    };
 
-    titleClick(item) {
-        console.log("点击的元素", item.name, item.id);
-    }
 
     /**
      * 刷新页面
      */
-    onRefreshLoaging() {
+    onRefreshLoading() {
         this.setState({isRef: true});
         setTimeout(() => {
             console.log("等待2s");
@@ -97,19 +141,21 @@ export default class Classification extends Component {
                 {/* 标题栏 */}
                 <View style={{flex: 1}}>
                     <ScrollView
-                        style={classStyle.classTitle}
+                        ref={'scrollView'}
+
                         showsHorizontalScrollIndicator={false}
                         horizontal={true}>
                         {this.titleData()}
                     </ScrollView>
+
                 </View>
                 {/* 数据 */}
-                <View style={{flex: 9}}>
+                <View style={{flex: 9, backgroundColor: "#ee2115"}}>
                     <ScrollView
                         refreshControl={<RefreshControl
                             refreshing={this.state.isRef}
                             onRefresh={
-                                this.onRefreshLoaging.bind(this)
+                                this.onRefreshLoading.bind(this)
                             }
                             tintColor='#fff'
                         />}>
@@ -120,80 +166,14 @@ export default class Classification extends Component {
         );
     }
 }
-const classStyle = StyleSheet.create({
-    backgroundVideo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-    },
-    infoImage: {
-        width: 30,
-        height: 30,
-        borderRadius: 50,
-        borderWidth: 2,
-        borderColor: "#000"
-    },
-    allImage: {
-        height: 10,
-        width: 10
-    },
-    parentVideo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-    },
-    childVideo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-    },
-    parent: {
-        borderColor: "#fff",
-        borderWidth: 1,
-
-    },
-    child: {
-        borderColor: "#ee2115",
-        borderWidth: 1,
-        marginTop: 5
-    },
-    outer: {
-        borderColor: "#9ec6ff",
-        borderWidth: 1
-    },
-    title: {
-        flex: 1,
-        backgroundColor: '#73c0ff',
-        textAlign: "center",
-        lineHeight: 40,
-        height: 40,
-        borderColor: "#ee2115",
-        borderWidth: 2
-    },
-
-
-    classText: {
-        height: 40,
-        lineHeight: 40,
-        backgroundColor: "#ff5fb2",
-        color: "#fff"
-    },
-    classTitle: {
-        backgroundColor: "#73c0ff",
-        maxHeight: 30
-    },
-    classTitleText: {
-        width: width / 4,
-        height: 30,
-        lineHeight: 30,
-        textAlign: "center",
-        borderColor: "#fff",
-        borderWidth: 1
-    }
-});
+const
+    classStyle = StyleSheet.create({
+        classTitleText: {
+            width: width / 4,
+            height: 50,
+            lineHeight: 50,
+            textAlign: "center",
+            borderColor: "#fff",
+            borderWidth: 1
+        }
+    });
