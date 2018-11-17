540eaf58c2c25726de60d3c653668943261da749
haogedada <2205584662@qq.com>
2018-11-17 12:43:05 +0800

    添加视频播放基础功能

--- App/pages/Video.js
+++ App/pages/Video.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'; 
 import {
     Text, View, StyleSheet, Dimensions, ScrollView, ViewPagerAndroid,
     DeviceEventEmitter
