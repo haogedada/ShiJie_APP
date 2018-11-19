8b3c98b93438129e8d50b0f5cf7f92def4716c4f
Net-Home <liuwu4@yeah.net>
2018-11-19 19:43:26 +0800

    添加逗号

--- android/app/src/main/java/com/shijie/MainApplication.java
+++ android/app/src/main/java/com/shijie/MainApplication.java
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNSyanImagePickerPackage(),
-            new ImagePickerPackage()
+            new ImagePickerPackage(),
             new ReactVideoPackage()
       );
     }
